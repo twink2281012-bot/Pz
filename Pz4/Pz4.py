97834bd094acf5d4b490050cbc33df2ebe66092e
Матвей <twink228_1012@mail.ru>
2026-03-16 21:02:03 +0300

    Update Pz4.py

--- Pz4/Pz4.py
+++ Pz4/Pz4.py
@@ -1,19 +1,22 @@
 # Дано вещественное число A и целое число N (>0). Найти A в степени N: AN = AA ...
 # •A (числа A перемножаются N раз)
 
-A = float(input("Введите вещественное число A: "))
-N = int(input("Введите целое число N (>0): "))
+try:
+    A = float(input("Введите вещественное число A: "))
+    N = int(input("Введите целое число N (>0): "))
 
-if N <= 0:
-    print("Ошибка: N должно быть больше 0!")
-else:
-    result = 1
+    if N <= 0:
+        print("Ошибка: N должно быть больше 0!")
+    else:
+        result = 1
 
-    count = 1
-    while count <= N:
-        result = result * A
-        count = count + 1
+        count = 1
+        while count <= N:
+            result = result * A
+            count = count + 1
 
-    print(A, "в степени", N, "=", result)
+        print(A, "в степени", N, "=", result)
 
-    print("Проверка через **:", A ** N)
+        print("Проверка через **:", A ** N)
+except ValueError:
+    print("Ошибка")
